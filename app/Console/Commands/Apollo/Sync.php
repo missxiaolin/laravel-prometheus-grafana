db38f1592eb8e7df30bfce63f9227a7055443999
xiaolin <462441355@qq.com>
2019-03-14 15:09:45 +0800

    * 增加redis存储

--- app/Console/Commands/Apollo/Sync.php
+++ app/Console/Commands/Apollo/Sync.php
@@ -8,6 +8,10 @@ use GuzzleHttp\Exception\RequestException;
 use Illuminate\Console\Command;
 use Symfony\Component\Yaml\Yaml;
 
+/**
+ * Class Sync
+ * @package App\Console\Commands\Apollo
+ */
 class Sync extends Command
 {
     /**
@@ -97,9 +101,21 @@ class Sync extends Command
     /**
      * @param $fileName
      * @param $item
+     * @throws \Exception
      */
     protected function save($fileName, $item)
     {
-        Storage::disk('custom')->put($fileName.'.yml', Yaml::dump($item));
+        if (config('apollo.sync.redis', false)) {
+
+            cache()->tags('apollo')->forever($fileName, $item);
+
+            $this->line('Saving To Redis ' . $fileName);
+
+        }
+        if (config('apollo.sync.file', false)) {
+            Storage::disk('custom')->put($fileName . '.yml', Yaml::dump($item));
+            $this->line('Saving To File ' . $fileName);
+        }
+        $this->line('==================');
     }
 }
