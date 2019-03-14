38433904174a77045fd063621fc8f121f309a0e4
xiaolin <462441355@qq.com>
2019-03-14 12:37:12 +0800

    * 取出valuse测试

--- app/Support/Apollo.php
+++ app/Support/Apollo.php
@@ -18,11 +18,43 @@ class Apollo
 {
     protected $config = [];
 
-
+    /**
+     * @param $key
+     * @param null $value
+     * @return mixed|null
+     */
     public function get($key, $value = null)
     {
+        $pos = strpos($key, '.');
+        $search = null;
 
-        dd($key);
+        //支持.查找
+        if ($pos) {
+            $name = substr($key, 0, $pos);
+            $search = ltrim(str_replace($name, '', $key), '.');
+        } else {
+            $name = $key;
+        }
+
+        $path = base_path('custom/' . $name . '.yml');
+
+        //读取文件
+        if (!isset($this->config[$name])) {
+            try {
+                $this->config[$name] = Yaml::parseFile($path);
+            } catch (\Exception $ex) {
+                return $value;
+            }
+        }
+
+
+        //直接返回
+        if (!$search) {
+            return $this->config[$name];
+        }
+
+        //从数组查找
+        return array_get($this->config[$name], $search, $value);
     }
 
 }
